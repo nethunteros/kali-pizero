605a66842924f70bbc7ce32a28cd5b1307663130
binkybear <binkybear@nehtunter.com>
2017-01-30 20:51:39 -0600

    Fixes

--- pi0.sh
+++ pi0.sh
@@ -11,13 +11,8 @@
 # Included:
 #
 # * Geneate SSH Keys on first boot
-# * XFCE4/Bash Tweaks from G0tMi1k and others
-#       > https://github.com/g0tmi1k/os-scripts/blob/master/kali-rolling.sh
-# * Change default lock screen
-# * rpi-config to allow you to expand rootfs
-# * Wireless packages
+
 # * VPN Packages
-# * MITM Packages
 # * re4son's PI  kernel
 #       > re4son: https://whitedome.com.au/re4son/sticky-fingers-kali-pi/#Vanilla
 #       > github: https://github.com/re4son/
@@ -63,17 +58,13 @@ xfce4="gtk3-engines-xfce lightdm-gtk-greeter-settings xfconf kali-desktop-xfce x
 tools="ethtool hydra john libnfc-bin mfoc nmap passing-the-hash php-cli sqlmap usbutils winexe tshark"
 services="openssh-server tightvncserver dnsmasq hostapd bridge-utils isc-dhcp-server dsniff screen"
 extras="unzip unrar curl firefox-esr xfce4-terminal wpasupplicant florence tcpdump dnsutils gcc build-essential"
-wireless="aircrack-ng cowpatty python-dev kismet wifite pixiewps wireless-tools wicd-curses"
 vpn="openvpn network-manager-openvpn network-manager-pptp network-manager-vpnc network-manager-openconnect network-manager-iodine"
-g0tmi1k="tmux ipcalc sipcalc psmisc htop gparted tor hashid p0f msfpc exe2hexbat windows-binaries thefuck burpsuite"
 
 # kernel sauces take up space yo.
 size=7000 # Size of image in megabytes
 
-packages="${arm} ${base} ${desktop} ${tools} ${services} ${extras} ${xfce4} ${vpn}"
-
-# Full
-#packages="${arm} ${base} ${desktop} ${tools} ${services} ${extras} ${mitm} ${wireless} ${xfce4} ${vpn} ${g0tmi1k}"
+# No Desktop...just commandline like a hax0r
+packages="${arm} ${base} ${desktop} ${xfce4} ${tools} ${services} ${extras} ${vpn}"
 
 # If you have your own preferred mirrors, set them here.
 # After generating the rootfs, we set the sources.list to the default settings.
@@ -115,18 +106,13 @@ ff02::2         ip6-allrouters
 EOF
 chmod 644 kali-$architecture/etc/hosts
 
-#cat << EOF > kali-$architecture/etc/network/interfaces
-#auto lo
-#iface lo inet loopback
+cat << EOF > kali-$architecture/etc/network/interfaces
+auto lo
 
-# This is to allow setup for Pi zero on address 1.0.0.1 which is compatible with poisontap
-#auto usb0
-#allow-hotplus usb0
-#iface usb0 inet static
-#  address 1.0.0.1
-#  netmask 0.0.0.0
-#EOF
-#chmod 644 kali-$architecture/etc/network/interfaces
+iface lo inet loopback
+iface eth0 inet dhcp
+EOF
+chmod 644 kali-$architecture/etc/network/interfaces
 
 cat << EOF > kali-$architecture/etc/resolv.conf
 nameserver 8.8.8.8
@@ -161,12 +147,11 @@ WantedBy=multi-user.target
 EOF
 chmod 755 kali-$architecture/lib/systemd/system/regenerate_ssh_host_keys.service
 
-# Copy Tweaks to tmp folder
-cp $TOPDIR/misc/xfce4-setup.sh kali-$architecture/tmp/xfce4-setup.sh
-cp $TOPDIR/misc/bashtweaks.sh kali-$architecture/tmp/bashtweaks.sh
-
 # Copy zram
 cp $TOPDIR/misc/zram kali-$architecture/etc/init.d/zram
+
+# cCopy tweaks
+cp $TOPDIR/misc/xfce4-setup.sh kali-$architecture/tmp/xfce4-setup.sh
 
 echo "[+] Begin THIRD STAGE"
 cat << EOF > kali-$architecture/third-stage
@@ -203,24 +188,54 @@ rm -f /etc/ssh/ssh_host_*_key*
 # image insecure and enable root login with a password.
 echo "[+] Making root great again"
 sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-# Fix startup time from 5 minutes to 15 secs on raise interface wlan0
-sed -i 's/^TimeoutStartSec=5min/TimeoutStartSec=15/g' "/lib/systemd/system/networking.service" 
-
 update-rc.d ssh enable
 
-############## Extra g0tmi1k apps ###############
+cat << EOF > /etc/systemd/system/rc-local.service.d/ttyoutput.conf  
+[Service]
+StandardOutput=tty
+EOF
+chmod 644 /etc/systemd/system/rc-local.service.d/ttyoutput.conf 
 
-# Fun MOTD
-echo "Connect through serial with on host computer: sudo screen /dev/ttyACM0 115200" | /usr/games/cowsay > /etc/motd
-
-# SSH Allow authorized keys
-sed -i 's/^#AuthorizedKeysFile /AuthorizedKeysFile /g' "/etc/ssh/sshd_config"  # Allow for key based login
-
-############################################################
 # Depends for rasp-config
 apt-get install -y libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils
 apt-get install -fy
+
+################################
+# Install poisontap and hackpi #
+################################
+echo "[+] Creating PI user"
+adduser pi --disabled-password --gecos "" --shell /bin/bash
+echo 'pi:raspberry' | chpasswd
+mkdir -p /home/pi
+cd /home/pi
+#git clone https://github.com/wismna/HackPi
+#chown -R pi HackPi
+#cd HackPi
+#cp isc-dhcp-server /etc/default/isc-dhcp-server
+#cp dhcpd.conf /etc/dhcp/dhcpd.conf
+#cp interfaces /etc/network/interfaces
+#cp modules /etc/modules
+#chmod +x *.sh
+#cd ..
+git clone https://github.com/samyk/poisontap.git
+chown -R pi poisontap
+cd poisontap
+cp dhcpd.conf /etc/dhcp
+cd ..
+echo "[+] Install nodejs"
+mkdir -p /tmp/node
+cd /tmp/node
+wget https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.4.0.sh
+chmod +x *.sh
+./install-node-v6.4.0.sh
+ln -s /lib/ld-linux.so.3 /lib/ld-linux-armhf.so.3
+cd /
+rm -rf /tmp/node
+
+# XFCE stuff (both users?)
+echo "[+] Running XFCE setup"
+chmod +x /tmp/xfce4-setup.sh
+/tmp/xfce4-setup.sh
 
 # Add Login Screen Tweaks
 # Add virtual keyboard to login screen
@@ -231,52 +246,11 @@ echo "keyboard=florence --focus" >> /etc/lightdm/lightdm-gtk-greeter.conf
 echo "background=/usr/share/images/desktop-base/kali-lockscreen_1280x1024.png" >> /etc/lightdm/lightdm-gtk-greeter.conf
 echo "default-user-image=#kali-k" >> /etc/lightdm/lightdm-gtk-greeter.conf
 
-################################
-# Install poisontap and hackpi #
-################################
-adduser pi --disabled-password
-echo 'pi:raspberry' | chpasswd
-mkdir -p /home/pi
-cd /home/pi
-git clone https://github.com/wismna/HackPi
-chown -R pi HackPi
-cd HackPi
-cp isc-dhcp-server /etc/default/isc-dhcp-server
-cp dhcpd.conf /etc/dhcp/dhcpd.conf
-cp interfaces /etc/network/interfaces
-chmod +x *.sh
-cd ..
-git clone https://github.com/samyk/poisontap.git
-chown -R pi poisontap
-cd poisontap
-cp dhcpd.conf /etc/dhcp
-cd ..
-git clone https://github.com/lgandx/Responder
-cd ..
-echo "[+] Install nodejs"
-mkdir -p /tmp/node
-cd /tmp/node
-wget https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.4.0.sh
-./install-node-v6.4.0.sh
-ln -s /lib/ld-linux.so.3 /lib/ld-linux-armhf.so.3
-cd /
-rm -rf /tmp/node
-
 # Raspi-config install
 echo "[+] Install raspi-config"
 cd /tmp
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20161207_all.deb
 dpkg -i raspi-config_*
-
-# XFCE stuff (both users?)
-echo "[+] Running XFCE setup"
-chmod +x /tmp/xfce4-setup.sh
-/tmp/xfce4-setup.sh
-
-
-echo "[+] Running bash tweaks"
-chmod +x /tmp/bashtweaks.sh
-/tmp/bashtweaks.sh
 
 rm -f /usr/sbin/policy-rc.d
 rm -f /usr/sbin/invoke-rc.d
@@ -307,14 +281,6 @@ chmod +x kali-$architecture/cleanup
 LANG=C chroot kali-$architecture /cleanup
 
 # Raspbian Configs worth adding
-
-#cat << EOF > kali-$architecture/etc/wpa_supplicant/wpa_supplicant.conf
-#country=GB
-#ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-#update_config=1
-#EOF
-#chmod 600 kali-$architecture/etc/wpa_supplicant/wpa_supplicant.conf
-
 cat << EOF > kali-$architecture/etc/apt/apt.conf.d/50raspi
 # never use pdiffs. Current implementation is very slow on low-powered devices
 Acquire::PDiffs "0";
@@ -470,7 +436,7 @@ git submodule update --init --recursive
 cd ${TOPDIR}/tools
 git pull
 # Kernel and firmware
-git clone --depth 1 https://github.com/nethunteros/re4son-raspberrypi-linux.git -b rpi-4.4.y-re4son ${basedir}/root/usr/src/kernel
+git clone --depth 1 https://github.com/nethunteros/re4son-raspberrypi-linux.git ${basedir}/root/usr/src/kernel
 cd ${basedir}/root/usr/src/kernel
 export ARCH=arm
 export KERNEL=kernel
@@ -478,8 +444,7 @@ export CROSS_COMPILE=${TOPDIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-
 
 # Make kernel with re4sons defconfig
 echo "[+] Building kernel and modules"
-#make ARCH=arm bcmrpi_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- re4son_pi1_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
 make -j $(grep -c processor /proc/cpuinfo) zImage modules dtbs
 
 echo "[+] Copying kernel"
@@ -494,14 +459,14 @@ make ARCH=arm firmware_install INSTALL_MOD_PATH=${basedir}/root
 echo "[+] Making kernel headers"
 make ARCH=arm headers_install INSTALL_HDR_PATH=${basedir}/root/usr
 
-# systemd doesn't seem to be generating the fstab properly for some people, so
-# let's create one.
+# gener
 cat << EOF > ${basedir}/root/etc/fstab
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
-proc /proc proc nodev,noexec,nosuid 0  0
-/dev/mmcblk0p2  / ext4 errors=remount-ro 0 1
-/dev/mmcblk0p1 /boot vfat noauto 0 0
+proc            /proc           proc    defaults          0       0
+/dev/mmcblk0p1  /boot           vfat    defaults          0       2
+/dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
 EOF
+chmod 644 {basedir}/root/etc/fstab
 
 # Unmount partitions
 umount -l $bootp
@@ -570,22 +535,19 @@ EOF
     echo "[+] Symlink to build"
     chroot $dir /bin/bash -c "chmod +x /tmp/fixkernel.sh"
 
+    echo "[+] Copying config.txt to /boot"
+    cp $TOPDIR/misc/config.txt $dir/boot/config.txt
+
     rm -f $dir/tmp/*
 
     # Create cmdline.txt file
     echo "[+] Creating /boot/cmdline.txt"
 cat << EOF > $dir/boot/cmdline.txt
-dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 elevator=deadline root=/dev/mmcblk0p2 rootfstype=ext4 rootwait net.ifnames=0
-EOF
-
-    # Create config.txt file
-    echo "[+] Creating /boot/config.txt"
-cat << EOF > $dir/boot/config.txt
-dtoverlay=dwc2
+dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait
 EOF
 
     # RC LOCAL
-cat << EOF > $dir/etc/rc.local
+#cat << EOF > $dir/etc/rc.local
 #!/bin/sh -e
 #
 # rc.local
@@ -600,51 +562,48 @@ cat << EOF > $dir/etc/rc.local
 # By default this script does nothing.
 
 # Print the IP address
-_IP=$(hostname -I) || true
-if [ "$_IP" ]; then
-  printf "My IP address is %s\n" "$_IP"
-fi
+#_IP=$(hostname -I) || true
+#if [ "$_IP" ]; then
+#  printf "My IP address is %s\n" "$_IP"
+#fi
 
 # Parse USB requests in dmesg
-/bin/bash /home/pi/HackPi/fingerprint.sh | tee /home/pi/os.txt
+#/bin/bash /home/pi/HackPi/fingerprint.sh | tee /home/pi/os.txt
 
 # Stop the dummy gadget and start the real one
-modprobe -r g_ether
-modprobe libcomposite
+#modprobe -r g_ether
+#modprobe libcomposite
 
 # libcomposite configuration
-/bin/sh /home/pi/HackPi/gadget.sh | tee /home/pi/HackPi/gadget.log
+#/bin/sh /home/pi/HackPi/gadget.sh | tee /home/pi/HackPi/gadget.log
 
 # Start bridge interface
-ifup br0
-ifconfig br0 up
+#ifup br0
+#ifconfig br0 up
 
 # Clear leases
 #rm -f /var/lib/dhcp/dhcpd.leases
 #touch /var/lib/dhcp/dhcpd.leases
 
 # Start the DHCP server
-/sbin/route add -net 0.0.0.0/0 br0
-/etc/init.d/isc-dhcp-server start
+#/sbin/route add -net 0.0.0.0/0 br0
+#/etc/init.d/isc-dhcp-server start
 # Set some other paramaters
-/sbin/sysctl -w net.ipv4.ip_forward=1
-/sbin/iptables -t nat -A PREROUTING -i br0 -p tcp --dport 80 -j REDIRECT --to-port 1337
+#/sbin/sysctl -w net.ipv4.ip_forward=1
+#/sbin/iptables -t nat -A PREROUTING -i br0 -p tcp --dport 80 -j REDIRECT --to-port 1337
 # Start some servers
 #/usr/bin/screen -dmS dnsspoof /usr/sbin/dnsspoof -i br0 port 53
-#/usr/bin/screen -dmS node /usr/bin/nodejs /home/pi/poisontap/pi_poisontap.js 
+#/usr/bin/screen -dmS node /usr/bin/nodejs /home/pi/poisontap/pi_poisontap.js
 
 # Enable Serial
-systemctl enable getty@ttyGS0.service
+#systemctl enable getty@ttyGS0.service
 
 # Start Responder
 #/usr/bin/screen -dmS responder bash -c 'cd /home/pi/Responder/; python Responder.py -I br0 -f -w -r -d -F'
 
-exit 0
-EOF
-chmod +x $dir/etc/rc.local
-
-    # Ethernet module
-    echo -e "dwc2\ng_ether" >> $dir/etc/modules
+#exit 0
+#EOF
+#chmod +x $dir/etc/rc.local
 
     # Enable regenerate ssh host keys at first boot
     chroot $dir /bin/bash -c "systemctl enable regenerate_ssh_host_keys"
@@ -693,3 +652,4 @@ else
         build_image
     fi
 fi
+
